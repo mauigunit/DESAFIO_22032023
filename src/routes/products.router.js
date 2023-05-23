eb07187cd0e46ecd451f7b903adbc2210f4d3b8e
Mauricio Garrido <mauigunit@gmail.com>
2023-05-22 20:38:11 -0400

    desafio 22032023

---
+++ src/routes/products.router.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+    getProdController,
+    getPIDController,
+    createProdController,
+    updateProdController, 
+    deleteProdController
+} from '../controllers/products.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js'
+import { authToken } from '../middlewares/jwt.js'
+
+const prodRouter = Router();
+
+prodRouter.get('/', authMiddleware, getProdController);
+prodRouter.get('/:pid', authMiddleware, getPIDController);
+prodRouter.post('/', authMiddleware, createProdController);
+prodRouter.put('/:pid', authMiddleware, updateProdController);
+prodRouter.delete('/:pid', authMiddleware, deleteProdController);
+
+export default prodRouter;
---
+++ src/routes/session.router.js
@@ -0,0 +1,19 @@
+import {Router} from 'express';
+import passport from 'passport';
+const sessionGithubRouter = Router();
+
+sessionGithubRouter.get('/github', passport.authenticate('github', {scope:['user:email']}), async (req, res) =>{});
+
+sessionGithubRouter.get('/githubcallback', passport.authenticate('github', {failureRedirect:'/login'}), async (req, res) =>{
+    req.session.user = req.user;
+    console.log(req.session.user);
+    //console.log("paso por aqui...");
+    res.redirect('/api/products');
+});
+
+sessionGithubRouter.get('/current', passport.authenticate('current', {session:false}), async (req, res) =>{
+    let user = req.user;
+    res.json({status: 'success', user: user})
+});
+
+export default sessionGithubRouter;
